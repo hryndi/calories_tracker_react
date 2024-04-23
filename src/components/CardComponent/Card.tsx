6242ea9a20a55df7979ee1c47c15df6f86db5974
Dmytro Hrynov <hrynovdmytro11@gmail.com>
2024-04-23 17:59:44 +0200

    adjust more logic in hook, passed passed hook logic to components.

--- src/components/CardComponent/Card.tsx
+++ src/components/CardComponent/Card.tsx
@@ -1,29 +1,53 @@
 import styled from "@emotion/styled";
-
-const CardWrapp = styled.div``;
+import { CardTypes } from "../../types";
+import Button from "../ButtonsComponent/Button";
+const CardsWrapp = styled.div`
+  display: flex;
+  gap: 1.5rem;
+`;
+const CardContent = styled.div``;
 const H2 = styled.h2``;
 const P = styled.p``;
 const BtnWrap = styled.div``;
 const BtnCountWrap = styled.div``;
 
-const Card = () => {
-  return (
-    <CardWrapp>
-      <H2>Apple</H2>
-      <P>Protein</P>
-      <P>Carbs</P>
-      <P>Fett</P>
+const Card = ({
+  cardValues,
+  plusMinusHandler,
+  editHandler,
+}: {
+  cardValues: CardTypes[];
+  plusMinusHandler: (action: string, id: string) => void;
+  editHandler: (id: string) => void;
+}) => {
+  console.log("CARDS");
 
-      <BtnWrap>
-        <BtnCountWrap>
-          <button>+</button>
-          <P>1</P>
-          <button>-</button>
-        </BtnCountWrap>
-        <button>Edit</button>
-        <button>Delete</button>
-      </BtnWrap>
-    </CardWrapp>
+  return (
+    <CardsWrapp>
+      {cardValues.map((item) => (
+        <CardContent>
+          <H2>{item.item_name}</H2>
+          <P>{item.protein}</P>
+          <P>{item.carbs}</P>
+          <P>{item.fat}</P>
+          <P>{item.calories}</P>
+
+          <BtnWrap>
+            <BtnCountWrap>
+              <Button onClick={() => plusMinusHandler("increace", item.id)}>
+                +
+              </Button>
+              <P>{item.quantity}</P>
+              <Button onClick={() => plusMinusHandler("decreace", item.id)}>
+                -
+              </Button>
+            </BtnCountWrap>
+            <Button onClick={() => editHandler(item.id)}>dwadwad</Button>
+            <Button>Delete</Button>
+          </BtnWrap>
+        </CardContent>
+      ))}
+    </CardsWrapp>
   );
 };
 
